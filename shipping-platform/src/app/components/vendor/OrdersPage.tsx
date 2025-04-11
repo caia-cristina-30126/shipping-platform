3e65740ca31d6f1b54f606c7cbbe325f219d207c
Cristina Caia <cristinaelenacaia@gmail.com>
2025-04-11 15:49:13 +0300

    #1 setting up ShadCN

--- shipping-platform/src/app/components/vendor/OrdersPage.tsx
+++ shipping-platform/src/app/components/vendor/OrdersPage.tsx
@@ -1,1 +1,62 @@
-export const OrdersPage = () => <div>Your orders are here</div>;
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export const OrdersPage = () => {
+  const formSchema = z.object({
+    name: z.string(),
+    description: z.string().nullable(),
+    quantity: z.number(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      quantity: 0,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log("save order");
+    console.log("values", values);
+  };
+
+  return (
+    <>
+      Create your Orders
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>This is your order name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+};
