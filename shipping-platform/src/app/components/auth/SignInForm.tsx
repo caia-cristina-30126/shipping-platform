b6373dd7052d052781ab50a3816dc6bfcd506a17
Cristina Caia <cristinaelenacaia@gmail.com>
2025-05-03 23:09:04 +0300

    #2 using Auth.js OAuth Google

---
+++ shipping-platform/src/app/components/auth/SignInForm.tsx
@@ -0,0 +1,15 @@
+
+import { signIn } from "@/auth"
+
+export const SignInForm = () => {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+  )
+} 
