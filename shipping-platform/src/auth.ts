b9f4f59744ea96935391c24aaa92e172994bbb56
Cristina Caia <cristinaelenacaia@gmail.com>
2025-05-10 16:01:32 +0300

    #2 using jwt strategy + middleware running in in a edge runtime

--- shipping-platform/src/auth.ts
+++ shipping-platform/src/auth.ts
@@ -8,7 +8,9 @@ import { prisma } from "./context";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: {strategy: 'database'},
+  session: {strategy: 'jwt',
+    maxAge: 5 * 24 * 60 * 60 // 5 days in seconds
+  },
   providers: [GoogleProvider({
     clientId: process.env.AUTH_GOOGLE_ID,
     clientSecret: process.env.AUTH_GOOGLE_SECRET
@@ -17,4 +19,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     clientId: process.env.AUTH_FACEBOOK_ID,
     clientSecret: process.env.AUTH_FACEBOOK_SECRET
   })],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      console.log("auth in callbacks", auth)
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth
+    },
+  },
 })
--- shipping-platform/src/middleware.ts
+++ shipping-platform/src/middleware.ts
@@ -1,2 +1,7 @@
 export { auth as middleware } from "@/auth"
 
+// middleware.js
+export const config = {
+    
+    matcher: ['/dashboard/(.*)',"/((?!api|_next/static|_next/image|favicon.ico|$).*)"],
+  };
