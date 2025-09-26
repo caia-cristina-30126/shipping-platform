94728c01ca78c481e10c53aa55c4f284d4945891
Cristina Caia <cristinaelenacaia@gmail.com>
2025-09-26 14:05:54 +0200

    updating header

--- shipping-platform/src/app/components/layout/Header.tsx
+++ shipping-platform/src/app/components/layout/Header.tsx
@@ -1,43 +1,42 @@
 "use client"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
     NavigationMenu,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
 } from "@/components/ui/navigation-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
-import { signOut } from "next-auth/react";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-
-type User = {
-    id?: string
-    name?: string | null
-    email?: string | null
-    image?: string | null
-};
+import { User } from "next-auth";
+import { redirect, usePathname } from "next/navigation";
 
 export default function Header({ user }: { user?: User }) {
+    const pathname = usePathname();
+    console.log("pathname", pathname)
+
+    const headerItems = [{ name: 'Products', url: 'products' }, { name: 'Orders', url: 'orders' }]
     return (
         <header className="w-full border-b px-6 py-4 bg-background flex items-center justify-between">
             <NavigationMenu>
                 <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="text-lg font-semibold" href="/">
-                            Home
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className="text-lg font-semibold" href="/dashboard" >
-                            Dashboard
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
+                    {headerItems.map((item, id) => {
+                        const href = `/dashboard/${item.url}`;
+                        const isActive =
+                            pathname === href || pathname.startsWith(`${href}/`);
+                        return <NavigationMenuItem key={id}>
+                            <NavigationMenuLink href={href} className={`text-lg font-semibold px-3 py-2 rounded-md transition-colors ${isActive ? "bg-primary text-white" : "hover:bg-muted"
+                                }`}>
+                                {item.name}
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    }
+
+                    )}
                 </NavigationMenuList>
             </NavigationMenu>
             <DropdownMenu>
